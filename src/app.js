3e059fa8db41c09fdd8f14b5f363836054feee3b
Kevin Wei <chenghaowei@gmail.com>
2019-09-29 13:54:30 -0400

    initial commit

---
+++ src/app.js
@@ -0,0 +1,47 @@
+/*******************************************************************
+  IMPORTS
+*******************************************************************/
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const validateBearerToken = require('./validateBearerToken');
+const errorHandler = require('./errorHandler');
+const bookmarksRouter = require('./bookmarks.router');
+
+/*******************************************************************
+  INIT
+*******************************************************************/
+const app = express();
+
+/*******************************************************************
+  MIDDLEWARE
+*******************************************************************/
+app.use(morgan(NODE_ENV === 'production' ? 'tiny' : 'common'));
+app.use(cors());
+app.use(helmet());
+//app.use(express.json()); //parses JSON data of req body
+app.use(validateBearerToken);
+
+/*******************************************************************
+  ROUTES
+*******************************************************************/
+app.use(bookmarksRouter);
+
+/*******************************************************************
+  ERROR HANDLING
+*******************************************************************/
+// Catch-all 404 handler
+app.use((req, res, next) => {
+  const err = new Error('Path Not Found');
+  err.status = 404;
+  next(err); // goes to errorHandler
+});
+app.use(errorHandler);
+
+/*******************************************************************
+  EXPORTS
+*******************************************************************/
+module.exports = app;
